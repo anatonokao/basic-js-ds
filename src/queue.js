7454bac7b93e5183b75c266a4d2710d07d80ffed
anatonokao <anato.devs@gmail.com>
2023-12-25 06:39:44 +0300

    finish stack,queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class Queue extends ListNode {
+  queue = {};
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    function pushToEnd(queueItem) {
+      queueItem.next
+        ? pushToEnd(queueItem.next)
+        : (queueItem.next = new ListNode(value));
+    }
+    if (!this.queue.value) this.queue = new ListNode(value);
+    else pushToEnd(this.queue);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const value = this.queue.value;
+    this.queue = this.queue.next;
+    return value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
